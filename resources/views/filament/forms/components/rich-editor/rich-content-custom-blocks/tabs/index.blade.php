db56d78830820d38a61a9677d8d18bcd3692b430
Arman <cit_arman@inbox.ru>
2026-08-13 17:01:26 +0500

    style: cursor-pointer

--- resources/views/filament/forms/components/rich-editor/rich-content-custom-blocks/tabs/index.blade.php
+++ resources/views/filament/forms/components/rich-editor/rich-content-custom-blocks/tabs/index.blade.php
@@ -6,9 +6,9 @@
         @foreach($tabs as $t)
             <button @click="tab = '{{ $t['id'] }}'"
                 :class="tab === '{{ $t['id'] }}' 
-                            ? 'text-yellow-600 shadow bg-white dark:text-white dark:bg-yellow-600' 
-                            : 'hover:text-gray-800 focus:text-yellow-600 dark:text-gray-400 dark:hover:text-gray-300 dark:focus:text-gray-400'"
-                class="flex whitespace-nowrap items-center h-8 px-2 md:px-5 font-medium rounded-lg outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-inset"
+                                ? 'text-yellow-600 shadow bg-white dark:text-white dark:bg-yellow-600' 
+                                : 'hover:text-gray-800 focus:text-yellow-600 dark:text-gray-400 dark:hover:text-gray-300 dark:focus:text-gray-400'"
+                class="flex whitespace-nowrap items-center h-8 px-2 md:px-5 font-medium rounded-lg outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-inset cursor-pointer"
                 role="tab" type="button" :aria-selected="tab === '{{ $t['id'] }}'">
                 {{ $t['title'] ?? '' }}
             </button>
